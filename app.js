faa6bf56ac7d6056dc71480c8ea8a379896c449f
jelius47 <jeliusheneriko47@gmail.com>
2024-12-17 14:05:39 +0300

    First edition of my personal portfolio website

--- app.js
+++ app.js
@@ -41,16 +41,36 @@
 
 // pageTransitions()
 
+// (function () {
+//     [...document.querySelectorAll(".control")].forEach(button => {
+//         button.addEventListener("click", function() {
+//             document.querySelector(".active-btn").classList.remove("active-btn");
+//             this.classList.add("active-btn");
+//             document.querySelector(".active").classList.remove("active");
+//             document.getElementById(button.dataset.id).classList.add("active");
+//         })
+//     });
+//     document.querySelector(".theme-btn").addEventListener("click", () => {
+//         document.body.classList.toggle("light-mode");
+//     })
+// })();
+
 (function () {
+    // Attach event listener for buttons and sections
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
+            // Switch active button
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
+
+            // Switch active section
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
-        })
+        });
     });
+
+    // Theme toggle functionality
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-    })
+    });
 })();
